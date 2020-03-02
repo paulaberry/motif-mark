6d6f1f76beebd025109e15352e9c4eb48d19885d
Paula Berry <pberry@uoregon.edu>
2020-03-01 23:00:38 -0800

    finished rough code

--- motif-mark.py
+++ motif-mark.py
@@ -4,7 +4,8 @@ import gzip
 import cairo
 import re
 import os
-from matplotlib import imshow
+import matplotlib
+from matplotlib import cm
 import numpy as np
 
 def get_args():
@@ -13,6 +14,7 @@ def get_args():
     getfiles.add_argument("-f", "--fastafile", help = "FASTA file to be parsed", required = True)
     getfiles.add_argument("-m", "--motiffile", help = "file with list of motifs to mark", required = True)
     getfiles.add_argument("-c", "--colormap", help = "specifies which matplotlib color map to use for motifs (default: hsv)", required = False, default = "hsv")
+    getfiles.add_argument("-o", "--output", help = "specifies output filetype (default:svg)", required = False, default = "svg")
     return getfiles.parse_args()
 args = get_args()
 
@@ -22,17 +24,14 @@ motiflist = str(args.motiffile)
 
 # optional arguments
 colormap = str(args.colormap)
-
-# function to create dictionary values for motif keys
-def values(motif):
-    """Function to create dictionary value for motif key"""
-    return regex_string(motif)
-    # returns regex and symbol/color for each motif
+filetype = str(args.output)
+if filetype.startswith(".") == False:
+    filetype = "." + filetype
 
 def regex_string(motif):
     """Function that returns a regular expression string to search for in the sequence from a motif."""
     motifstring = motif.strip()
-    value_regex = "(?i"
+    value_regex = "(?i)"
     for i in motifstring:
         if i == "T" or i == "t" or i == "U" or i == "u":
             value_regex = value_regex + "[TU]"
@@ -66,14 +65,36 @@ def regex_string(motif):
             value_regex = value_regex + "[AGCTUN]"
         else:
             value_regex = value_regex + i
-    value_regex = value_regex + ")"
+    #value_regex = value_regex + ")"
     return(value_regex)
 
+def exonlength(sequence):
+    """A function that takes in a FASTA sequence as a string and returns the coordinates for the exon start and stop"""
+    m = re.search("[A-Z]+", sequence)
+    ex_start = int((m.start() / len(sequence)) * 1000)
+    ex_end = int((m.end() / len(sequence)) * 1000)
+    return ex_start, ex_end
 
-# color values dictionary
-def pick_colors(n, cmap):
-    """A function that takes in as arguments the total number of motifs and the colormap desired and returns RGB values as tuples"""
-    return cm.cmap(n)
+def motif_location(motif, sequence, dictionary):
+    """A function that takes in a motif, a FASTA sequence as a string, and an empty list to populate with the coordinates of the motif found."""
+    coord_list = []
+    p = re.compile(dictionary[key][0])
+    for match in p.finditer(sequence):  # Search for the motif in the sequence
+        coord_list.append(match.start())
+    #print(coord_list)
+    if coord_list == []:
+        #print(coord_list)
+        return coord_list
+    else:
+        list_count = 0
+        for i in coord_list:
+            scaled_coord = int((i / len(sequence)) * 1000)
+            coord_list[list_count] = scaled_coord
+            list_count = list_count + 1
+        #print(coord_list)
+        return coord_list
+
+
 
 # initialize motif dictionary
 motif_dictionary = {}
@@ -89,33 +110,140 @@ for motif in motif_file:
 motif_file.close()
 
 # Populate dictionary with RGB value tuples
-color_step = 255 % motif_count
+color_step = 255 // motif_count
 color_n = 0
 for key in motif_dictionary:
-    motif_dictionary[key].append = pick_colors(color_n, cmap)
+    motif_dictionary[key].append(getattr(cm, colormap)(color_n))
     color_n = color_n + color_step
 
-print(motif_dictionary)
+#print(motif_dictionary)
 
 
 
 # Input FASTA file, count fasta entries
-fasta_file = open(fastafile)
+fasta_file = open(fastafile, "r")
 fasta_count = 0
 fasta_line = fasta_file.readline(0)
-for line in fasta_line:
-    line = line.strip()
-    if line.startswith == ">":
+for fasta_line in fasta_file:
+    fasta_line = fasta_line.strip()
+    if fasta_line.startswith(">"):
         fasta_count = fasta_count + 1
 fasta_file.close()
 
-print(fasta_count)
+#print(fasta_count)
 
 # Initialize canvas
-#canvas_height = fasta_count * 100 + 100
-#svg_filename = str(os.path.splitext(fastafile)[0]) + "_motifs.svg"
-#with cairo.SVGSurface(svg_filename, 1100, canvas_height) as surface:
-    #context = cairo.Context(surface)
+canvas_height = (fasta_count * 100) + 160 + (30 * motif_count)
+img_filename = str(os.path.splitext(fastafile)[0]) + "_motifs" + filetype
+
+with cairo.SVGSurface(img_filename, 1100, canvas_height) as surface:
+    context = cairo.Context(surface)
+    context.set_source_rgb(0, 0, 0)
+    context.select_font_face("Georgia", cairo.FONT_SLANT_NORMAL,
+        cairo.FONT_WEIGHT_NORMAL)
+    context.set_font_size(13)
+    context.move_to(20, 30)
+    context.show_text("Motif Legend")
+    legend_line = 30
+    for key in motif_dictionary: # create legend with all motifs and colors
+        legend_line = legend_line + 30
+        context.set_source_rgb(motif_dictionary[key][1][0], motif_dictionary[key][1][1], motif_dictionary[key][1][2])
+        context.rectangle(20, (legend_line-15), 50, 20)
+        context.fill()
+        context.set_source_rgb(0, 0, 0)
+        context.move_to(80, legend_line)
+        context.show_text(key)
+
+    fasta = open(fastafile, "r") # start reading in fasta entries
+    sequence = ""
+    counter = 0
+    draw_line = legend_line + 100
+    for line in fasta:
+        line = line.strip()
+        if counter == 0: # special case for first loop through
+            header = line[1:]
+            counter = counter + 1
+
+        elif line[0] == ">" or line == "": # here's where we start drawing
+            # generate coordinates for exon
+            exon_start, exon_stop = exonlength(sequence)
+
+            # draw using the coordinates
+            draw_strand = int(draw_line + 30)
+            context.set_source_rgb(0, 0, 0)
+            context.move_to(20, draw_line)
+            context.show_text(header)
+            context.move_to(50, draw_strand)
+            context.line_to(1050, draw_strand)
+            context.set_source_rgba(0, 0, 0, .7)
+            context.set_line_width(5)
+            context.stroke()
+            context.move_to(50 + exon_start, draw_strand)
+            context.line_to(50 + exon_stop, draw_strand)
+            context.set_source_rgb(0, 0, 0)
+            context.set_line_width(20)
+            context.stroke()
+
+            # generate list of motif coordinates
+            for key in motif_dictionary:
+                motif_len = int((len(key) / len(sequence)) * 1000)
+                #print(motif_len)
+                motif_coords = motif_location(key, sequence, motif_dictionary)
+                if motif_coords != []:
+                    for i in motif_coords:
+                        context.move_to(50 + i, draw_strand)
+                        context.line_to(50 + i + motif_len, draw_strand)
+                        context.set_source_rgba(motif_dictionary[key][1][0], motif_dictionary[key][1][1], motif_dictionary[key][1][2], .7)
+                        context.set_line_width(20)
+                        context.stroke()
+
+
+            draw_line = draw_line + 100
+            sequence = "" # clear the sequence
+            header = line[1:]
+
+        else: # build the sequence
+            sequence = sequence + line
+    fasta.close()
+    # generate coordinates for last exon
+    exon_start, exon_stop = exonlength(sequence)
+
+    # draw using the coordinates
+    draw_strand = int(draw_line + 30)
+    context.set_source_rgb(0, 0, 0)
+    context.move_to(20, draw_line)
+    context.show_text(header)
+    context.move_to(50, draw_strand)
+    context.line_to(1050, draw_strand)
+    context.set_source_rgba(0, 0, 0, .7)
+    context.set_line_width(5)
+    context.stroke()
+    context.move_to(50 + exon_start, draw_strand)
+    context.line_to(50 + exon_stop, draw_strand)
+    context.set_source_rgb(0, 0, 0)
+    context.set_line_width(20)
+    context.stroke()
+
+    # generate list of motif coordinates
+    for key in motif_dictionary:
+        motif_len = int((len(key) / len(sequence)) * 1000)
+        #print(motif_len)
+        motif_coords = motif_location(key, sequence, motif_dictionary)
+        if motif_coords != []:
+            for i in motif_coords:
+                context.move_to(50 + i, draw_strand)
+                context.line_to(50 + i + motif_len, draw_strand)
+                context.set_source_rgba(motif_dictionary[key][1][0], motif_dictionary[key][1][1], motif_dictionary[key][1][2], .7)
+                context.set_line_width(20)
+                context.stroke()
+
+#if img_filename[-3] == "png":
+    #surface.write_to_png(img_filename)
+#elif img_filename[-3] == "pdf":
+
+
+surface.finish()
+
 # Loop here
     # input FASTA entry
         #Create single line FASTA
--- motif-mark.sh
+++ motif-mark.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-conda activate pycairo
-./motif-mark.py -f test.fa -m Fig_1_motifs.txt
+./motif-mark.py -f test.fa -m Fig_1_motifs.txt -c jet
